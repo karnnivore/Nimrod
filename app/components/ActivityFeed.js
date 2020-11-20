8e1ce6a857b6e9d4f6cfba156d9a9a25e9c0b6aa
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-19 22:32:11 -0500

    Added basics for Activity Feed Page

--- app/components/ActivityFeed.js
+++ app/components/ActivityFeed.js
@@ -3,7 +3,9 @@ import {
     StyleSheet, 
     Text, 
     View,
-    Button
+    Button,
+    FlatList,
+    Image
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -11,7 +13,30 @@ import Icon from 'react-native-vector-icons/Ionicons';
 export default function ActivityFeed() {
     return (
       <View>
-          <Text>Activity Feed</Text>
+          <Text>
+            <Text>Activity Feed</Text>
+            <Icon name={'md-people'} color={'black'} size={30}/>
+          </Text>
+          <FlatList
+            data={[
+                {key: 'Nick', location: 'Toronto', questStatus: 'Completed', time:'5 MINS AGO'},
+                {key: 'June', location: 'Ottawa', questStatus: 'Completed', time:'15 MINS AGO'},
+                {key: 'Max', location: 'Toronto', questStatus: 'Completed', time:'23 MINS AGO'},
+                {key: 'Kevin', location: 'Toronto', questStatus: 'Completed', time:'30 MINS AGO'},
+                {key: 'Georgina', location: 'Vancouver', questStatus: 'Completed', time:'45 MINS AGO'},
+            ]}
+            renderItem={({item}) => 
+                <Text style={styles.activity}>
+                    <Icon name={'md-people'} color={'black'} size={80}/>
+                    <Text style={styles.person}>
+                        <Text>{item.key}</Text>
+                        <Text>{item.location}</Text>
+                        <Text>{item.questStatus}</Text>
+                    </Text>
+                    <Text>{item.time}</Text>
+                </Text>
+            }
+          />
       </View>
     )
 }
@@ -28,4 +53,14 @@ ActivityFeed.navigationOptions = {
 }
 
 const styles = StyleSheet.create({
+    activity: {
+        flex: 1,
+        backgroundColor: 'green',
+        minHeight: 100,
+        marginBottom: 10,
+        padding: 20,
+    },
+    person: {
+        marginLeft: 30,
+    }
 });
