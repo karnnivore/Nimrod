2c4aa4dcce67155ae17ae0eeaa437e2d3da0ab51
Nick Chinsen <nicholaschinsen@gmail.com>
2020-11-27 19:17:10 -0500

    Added BottomNav to replace initial Router

---
+++ routes/BottomNav.js
@@ -0,0 +1,18 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import HomeScreen from '../app/components/Home';
+import MapScreen from '../app/components/Map';
+import QuestScreen from '../app/components/Quests';
+import ActivityScreen from '../app/components/ActivityFeed';
+
+const Tab = createMaterialBottomTabNavigator();
+
+function BottomTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen}/>
+            <Tab.Screen name="Map" component={MapScreen}/>
+            <Tab.Screen name="Quests" component={QuestScreen}/>
+            <Tab.Screen name="Feed" component={ActivityScreen}/>
+        </Tab.Navigator>
+    )
+}
